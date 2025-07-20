4b2f8fa866f3db5ae980ad65945c5268e8ad8b7c
kaiboyboy <btele303@gmail.com>
2025-07-20 23:49:39 +0800

    Add files via upload

---
+++ Receptionist/Function/Func_SubMng.java
@@ -0,0 +1,106 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Receptionist.EMM;
+
+import File_Mng.SaveFile;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Vector;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author SCSM11
+ */
+public class Func_SubMng {
+    
+    public static void search(JTable table,JFrame parentFrame,JTextField in_SN){
+        String SN = in_SN.getText().trim();
+        boolean found = false;
+        
+        if (SN.isEmpty()){
+            JOptionPane.showMessageDialog(parentFrame,"Please enter a Student Name.");
+            return;
+        }
+        
+        DefaultTableModel model = (DefaultTableModel) table.getModel();
+        model.setRowCount(0);
+        
+        try (BufferedReader br = new BufferedReader (new FileReader("SER.txt"))){
+            String line = br.readLine();
+            
+            while ((line = br.readLine()) != null){
+                String [] rowData = line.split(",",-1);
+                
+                if (rowData[0].toLowerCase().contains(SN.toLowerCase())){
+                    Vector<Object> row = new Vector<>();
+                    found = true;
+                    
+                    for (int k = 0; k < rowData.length;k++){
+                        row.add(rowData[k]);
+                    }
+                    model.addRow(row);
+                }
+            }
+            if (found == false){
+                JOptionPane.showMessageDialog(parentFrame,"Student not found...");
+            }
+            
+        } catch (Exception e){
+            JOptionPane.showMessageDialog(parentFrame,"Error: " + e.getMessage());
+        }
+        
+        in_SN.setText("");
+    }
+    
+    
+    public static void save(
+        JTextField in_name,
+        JTextField in_old_subject,
+        JTextField in_new_subject,
+        JComboBox<String> in_status,
+        JTable table,
+        JFrame parentFrame){
+        
+        String name = in_name.getText();
+        String old_subject = in_old_subject.getText();
+        String new_subject = in_new_subject.getText();
+        String status = (String) in_status.getSelectedItem();
+
+        int selectedRow = table.getSelectedRow();
+
+        if (selectedRow == -1){
+            JOptionPane.showMessageDialog(parentFrame,"Please select a row to update");
+            return;
+        }
+
+        if (in_status.getSelectedIndex() == 0) {
+            JOptionPane.showMessageDialog(parentFrame,"Please Update Status");
+            return;
+        }
+
+        DefaultTableModel model = (DefaultTableModel) table.getModel();
+        model.removeRow(selectedRow);
+
+        model.addRow(new Object[] {
+            name,old_subject,new_subject,status
+        });
+
+        SaveFile.SERFile(table,parentFrame);
+
+        in_name.setText("");
+        in_old_subject.setText("");
+        in_new_subject.setText("");
+        in_status.setSelectedIndex(0);
+
+        JOptionPane.showMessageDialog(parentFrame,"Save Successfully");
+    }
+    
+}
